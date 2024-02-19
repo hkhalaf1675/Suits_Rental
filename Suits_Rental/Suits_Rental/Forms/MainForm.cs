75bc89349f38b0d0e4b712d78c9629bbcf5a441f
Hassan Tawfik <58273369+hkhalaf1675@users.noreply.github.com>
2024-02-19 12:36:45 -0800

    add main form

---
+++ Suits_Rental/Suits_Rental/Forms/MainForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Suits_Rental.Forms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnMaxmize_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Maximized)
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+            else
+            {
+                Rectangle rect = Screen.FromHandle(this.Handle).WorkingArea;
+                rect.Location = new Point(0, 0);
+                this.MaximizedBounds = rect;
+                this.WindowState = FormWindowState.Maximized;
+            }
+        }
+
+        private void btnMinmize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
--- Suits_Rental/Suits_Rental/Program.cs
+++ Suits_Rental/Suits_Rental/Program.cs
@@ -1,3 +1,5 @@
+using Suits_Rental.Forms;
+
 namespace Suits_Rental
 {
     internal static class Program
@@ -11,7 +13,7 @@ namespace Suits_Rental
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
