d45c20d01154bb1280ef99d884dea5fd8211f2d9
MuhammedAli11234 <mohamed1321997@gmail.com>
2024-03-06 23:09:57 +0200

    update suits

--- Suits_Rental/Suits_Rental/obj/Debug/net7.0-windows/Suits_Rental.AssemblyInfo.cs
+++ Suits_Rental/Suits_Rental/obj/Debug/net7.0-windows/Suits_Rental.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Suits_Rental")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+874b0d3cb455a95f5aecde00abaf9d204e517ade")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b564d71862c3e10abc4cc14625ec4defa7500fd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Suits_Rental")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Suits_Rental")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
